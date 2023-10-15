f4637fa3c623024649b1e2458c5b4c145a4ff37a
Pavel Tolstikov <ne2pit@gmail.com>
2023-10-15 13:56:48 +0300

    Share http.Client, bench ContextRefresh, bench ContextTreatment with and without Close()

--- pkg/benchmark/bench_test.go
+++ pkg/benchmark/bench_test.go
@@ -3,7 +3,7 @@ package benchmark
 import (
 	"fmt"
 	"os"
-	"strconv"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,16 +11,21 @@ import (
 	"github.com/absmartly/go-sdk/sdk"
 )
 
+var (
+	clients   = make(map[string]sdk.ClientI)
+	clientsMu sync.Mutex
+)
+
 func getContext() *sdk.Context {
-	return getContextWithUID(747)
+	return getContextWithUID("user", 747)
 }
 
-func getContextWithUID(uid int) *sdk.Context {
+func getContextWithUID(prefix string, uid int) *sdk.Context {
 	endpoint := os.Getenv("ABSMARTLY_ENDPOINT")
-	return getContextWithParams(endpoint, uid)
+	return getContextWithParams(endpoint, fmt.Sprintf("%s-%d", prefix, uid))
 }
 
-func getContextWithParams(endpoint string, uid int) *sdk.Context {
+func getContextWithParams(endpoint string, uid string) *sdk.Context {
 	var clientConfig = sdk.ClientConfig{
 		Endpoint_:    endpoint,
 		ApiKey_:      os.Getenv("ABSMARTLY_APIKEY"),
@@ -28,15 +33,22 @@ func getContextWithParams(endpoint string, uid int) *sdk.Context {
 		Environment_: os.Getenv(`ABSMARTLY_ENV`),
 	}
 
+	clientsMu.Lock()
+	if cli, ok := clients[endpoint]; !ok {
+		cli = sdk.CreateDefaultClient(clientConfig)
+		clients[endpoint] = cli
+	}
+	cli := clients[endpoint]
+	clientsMu.Unlock()
 	var sdkConfig = sdk.ABSmartlyConfig{
-		Client_: sdk.CreateDefaultClient(clientConfig),
+		Client_: cli,
 	}
 
 	var sd = sdk.Create(sdkConfig)
 
 	var contextConfig = sdk.ContextConfig{
 		Units_: map[string]string{ // add Unit
-			"user_id": strconv.Itoa(uid),
+			"user_id": uid,
 		}, PublishDelay_: 10000, RefreshInterval_: 5000}
 
 	var ctx = sd.CreateContext(contextConfig)
@@ -53,9 +65,17 @@ func BenchmarkGetContext(b *testing.B) {
 
 func BenchmarkGetContextBadEndpoint(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		c := getContextWithParams("http://127.0.0.1:79", 747)
+		c := getContextWithParams("http://127.0.0.1:79", "any-747")
 		c.WaitUntilReady()
 		c.Close()
+	}
+}
+
+func BenchmarkContextRefresh(b *testing.B) {
+	c := getContext()
+	c.WaitUntilReady()
+	for i := 0; i < b.N; i++ {
+		c.Refresh()
 	}
 }
 
@@ -131,15 +151,31 @@ func BenchmarkGetTreatment(b *testing.B) {
 }
 
 func BenchmarkContextTreatment(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		c := getContextWithUID(i)
-		c.WaitUntilReady()
-		_, err := c.GetTreatment("filter-test")
-		assert.NoError(b, err)
-		err = c.Publish()
-		assert.NoError(b, err)
-		if b.Failed() {
-			return
+	b.Run("close", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			c := getContextWithUID("close", i)
+			c.WaitUntilReady()
+			_, err := c.GetTreatment("filter-test")
+			assert.NoError(b, err)
+			err = c.Publish()
+			assert.NoError(b, err)
+			if b.Failed() {
+				return
+			}
+			c.Close()
 		}
-	}
+	})
+	b.Run("noclose", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			c := getContextWithUID("noclose", i)
+			c.WaitUntilReady()
+			_, err := c.GetTreatment("filter-test")
+			assert.NoError(b, err)
+			err = c.Publish()
+			assert.NoError(b, err)
+			if b.Failed() {
+				return
+			}
+		}
+	})
 }
